3ff412abe730ef84e5a92b60f8d60b868026b00d
henson lee <hensonlee1022@gmail.com>
2026-01-29 21:04:44 +0800

    added instantiation of export vars in player character that i forgot initially

--- flappy-opera/game.gd
+++ flappy-opera/game.gd
@@ -2,9 +2,14 @@ extends Node2D
 var score = 0
 @onready var  char= load("res://OperaO.tscn")
 @onready var main = get_tree().get_root().get_node("gameplay")
+@export var gameover: Label
+@export var restart: Button
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	var instance = char.instantiate()
+	instance.gameover = gameover
+	instance.restart = restart
 	instance.global_position =  Vector2(404, 223)
 	main.add_child.call_deferred(instance)
 	pass # Replace with function body.
